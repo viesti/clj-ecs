8e3caf13b86a0c9a5075125eb4e7d4d7f8a4ed43
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2019-11-10 16:34:25 +0200

    Log URI as JSON

--- src/clj_ecs/core.clj
+++ src/clj_ecs/core.clj
@@ -1,13 +1,16 @@
 (ns clj-ecs.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [timbre-json-appender.core :as tas]
+            [taoensso.timbre :as timbre]))
 
-(defn handler [_]
-  (println "Awesome")
+(defn handler [{:keys [uri]}]
+  (timbre/info :uri uri)
   {:status  200
    :headers {"Content-Type" "text/plain"}
    :body    "Hello World!"})
 
 (defn -main [& _]
+  (tas/install)
   (jetty/run-jetty handler
                    {:port (or (some-> (System/getenv "PORT")
                                       (Integer/parseInt))
