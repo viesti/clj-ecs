d1aa6d43935563079d0499e536797ab0088c3ab8
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2019-11-10 16:34:41 +0200

    Tag image with Git SHA

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+GIT_SHA=$(git rev-parse --short HEAD)
 clj -A:pack \
     -m mach.pack.alpha.jib \
-    --image-name 678497885140.dkr.ecr.eu-west-1.amazonaws.com/backend:2 \
+    --image-name 678497885140.dkr.ecr.eu-west-1.amazonaws.com/backend:$GIT_SHA \
     --image-type registry \
     -m clj-ecs.core
